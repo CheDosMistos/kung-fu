8a7b88d080f0b6289d18bfd1165a9fde5fcec565
Alejandro Mallo <chedosmistos@gmail.com>
2022-06-07 20:11:54 +0200

    recolocar un par de variables

--- Javascript/002.palindrome/app.js
+++ Javascript/002.palindrome/app.js
@@ -1,9 +1,7 @@
-let isPalindrome = true; 
-let step = 0;
-
 const arguments = process.argv.slice(2);
 const sentence = arguments[0];
 
+let step = 0;
 while (sentence[step]!== undefined) {
     step ++;
 }
@@ -12,6 +10,7 @@ const firstLetterIndex = 0;
 const lastLetterIndex = step - 1;
 step = 0;
 
+let isPalindrome = true;
 while (isPalindrome===true && lastLetterIndex-step>=firstLetterIndex+step) {
     if (sentence[firstLetterIndex+step] !== sentence[lastLetterIndex-step]) {
         isPalindrome = false;
