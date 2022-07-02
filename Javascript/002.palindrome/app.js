769a2902d834c649322e005f986494099677943b
Alejandro Mallo <chedosmistos@gmail.com>
2022-07-02 22:49:24 +0200

    Litle fixes arownd the old JavaScript execices

--- Javascript/002.palindrome/app.js
+++ Javascript/002.palindrome/app.js
@@ -2,7 +2,7 @@ const arguments = process.argv.slice(2);
 const sentence = arguments[0];
 
 let step = 0;
-while (sentence[step]!== undefined) {
+while (sentence[step] !== undefined) {
     step ++;
 }
 
